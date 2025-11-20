1e1d976fadf222083dd888bfd8989f0802f9898b
Gul Badeen <golbadeen@gmail.com>
2025-11-20 10:11:28 +0500

    server added

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  compress: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
